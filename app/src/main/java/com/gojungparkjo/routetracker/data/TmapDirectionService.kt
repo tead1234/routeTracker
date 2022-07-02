0a33415abb05a4210ff49341ed4ccdbc6d9aad41
jibminJung <jibminjung@gmail.com>
2022-07-03 00:15:07 +0900

    TmapDirection 쿼리파라미터 수정

--- app/src/main/java/com/gojungparkjo/routetracker/data/TmapDirectionService.kt
+++ app/src/main/java/com/gojungparkjo/routetracker/data/TmapDirectionService.kt
@@ -7,5 +7,5 @@ import retrofit2.http.Query
 
 interface TmapDirectionService {
     @POST("routes/pedestrian?version=1")
-    suspend fun getDirection(@Query("startX") lat:Double, @Query("startY") lon:Double, @Query("endX") lat2:Double, @Query("endY") lon2:Double, @Query("startName") starting: String, @Query("startName") ending: String, @Query("appKey") key:String): Response<TmapDirection>
+    suspend fun getDirection(@Query("startX") lat:Double, @Query("startY") lon:Double, @Query("endX") lat2:Double, @Query("endY") lon2:Double, @Query("startName") starting: String, @Query("endName") ending: String, @Query("appKey") key:String): Response<TmapDirection>
 }
